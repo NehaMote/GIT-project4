04a6340cc1f413061ca8cece0d0780970aa1f196
NehaMote <nehamote18@gmail.com>
2024-05-10 10:24:39 +0530

    add project4

---
+++ src/com/sit/git/project3/Addition.java
@@ -0,0 +1,11 @@
+package com.sit.git.project3;
+
+public class Addition {
+	 public static void add() {
+		 
+	 }
+	public static void main(String[] args) {
+		add();
+	}
+
+}
---
+++ src/com/sit/git/project3/Test.java
@@ -0,0 +1,8 @@
+package com.sit.git.project3;
+
+public class Test {
+	public static void main(String[] args) {
+		Addition.add();
+	}
+
+}
